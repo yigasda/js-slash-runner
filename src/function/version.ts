ccdc67c2aff77daff7e003dd4d7fef83c799514b
yigasda <143778990+yigasda@users.noreply.github.com>
2025-11-30 01:27:57 +0900

    Add files via upload

--- src/function/version.ts
+++ src/function/version.ts
@@ -1,15 +1,17 @@
-import manifest from '@/../manifest.json';
-import { getTavernHelperExtensionId, updateExtension } from '@/function/extension';
-import { version } from '@/util/tavern';
+import {
+  getCurrentVersion,
+  updateTavernHelper as updateTavernHelperImpl,
+  VERSION_FILE_PATH,
+} from '@/util/check_update';
 
-export function getTavernHelperVersion(): string {
-  return manifest.version;
+export async function getTavernHelperVersion(): Promise<string> {
+  const currentVersion = await getCurrentVersion(VERSION_FILE_PATH);
+  if (typeof currentVersion !== 'string') {
+    throw new Error('가져온 버전 번호가 유효하지 않습니다.');
+  }
+  return currentVersion;
 }
 
-export async function updateTavernHelper(): Promise<boolean> {
-  return updateExtension(getTavernHelperExtensionId()).then(res => res.ok);
-}
-
-export function getTavernVersion(): string {
-  return version;
+export async function updateTavernHelper() {
+  return updateTavernHelperImpl();
 }
